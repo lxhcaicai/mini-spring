4d40ef50784e60025290d0ed04c56ac07adf987c
lxhcaicai <2778763221@qq.com>
2024-08-09 01:08:59 +0800

    step08 BeanFactoryPostProcessor和BeanPostProcessor

--- src/main/java/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
@@ -3,4 +3,6 @@ package org.springframework.beans.factory.config;
 import org.springframework.beans.factory.HierarchicalBeanFactory;
 
 public interface ConfigurableBeanFactory extends HierarchicalBeanFactory,SingletonBeanRegistry {
+
+    void addBeanPostProcessor(BeanPostProcessor beanPostProcessor);
 }
--- src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -2,9 +2,15 @@ package org.springframework.beans.factory.support;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements ConfigurableBeanFactory {
+
+    private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<>();
 
     @Override
     public Object getBean(String name) throws BeansException {
@@ -26,4 +32,13 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
 
     protected abstract BeanDefinition getBeanDefinition(String beanName) throws BeansException;
 
+    @Override
+    public void addBeanPostProcessor(BeanPostProcessor beanPostProcessor) {
+        this.beanPostProcessors.remove(beanPostProcessor);
+        this.beanPostProcessors.add(beanPostProcessor);
+    }
+
+    public List<BeanPostProcessor> getBeanPostProcessors() {
+        return beanPostProcessors;
+    }
 }
