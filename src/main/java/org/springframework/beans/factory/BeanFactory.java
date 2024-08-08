39809dcd85af0c51d3168d4097f06db3ed13dd11
lxhcaicai <2778763221@qq.com>
2024-08-08 20:57:20 +0800

    step02 BeanDefinition和BeanDefinitionRegistry

--- src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -1,16 +1,9 @@
 package org.springframework.beans.factory;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.beans.BeansException;
 
-public class BeanFactory {
-    private Map<String,Object> beanMap = new HashMap<>();
 
-    public void registerBean(String name, Object bean) {
-        beanMap.put(name, bean);
-    }
+public interface BeanFactory {
 
-    public Object getBean(String name) {
-        return beanMap.get(name);
-    }
+    Object getBean(String name) throws BeansException;
 }
---
+++ src/main/java/org/springframework/beans/factory/support/BeanDefinitionRegistry.java
@@ -0,0 +1,16 @@
+package org.springframework.beans.factory.support;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+
+/**
+ * BeanDefinition注册表接口
+ */
+public interface BeanDefinitionRegistry {
+
+    /**
+     * 向注册表中注BeanDefinition
+     * @param beanName
+     * @param beanDefinition
+     */
+    void registerBeanDefinition(String beanName, BeanDefinition beanDefinition);
+}
